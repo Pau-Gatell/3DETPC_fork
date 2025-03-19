b410336fc21a142673bc3958b8cde236b111066c
Vostek <alberto93.dev@gmail.com>
2025-03-19 09:27:47 +0100

    feat(events): player using events for UI updates.

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerHealthController : MonoBehaviour
 {
     public float health = 100f;
 
+    public static UnityEvent<float> damageEvent;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,15 +23,17 @@ public class PlayerHealthController : MonoBehaviour
     public void TakeDamage(float dmg)
     {
         health = health + dmg;
-        UIHealth uiHealth = FindObjectOfType<UIHealth>();
-        uiHealth.UpdateHealth(health);
+
+        damageEvent.Invoke(health);
+        //UIHealth uiHealth = FindObjectOfType<UIHealth>();
+        //uiHealth.UpdateHealth(health);
     }
 
     // Regenerate health
     public void RegenHealth(float hp)
     {
         health = health + hp;
-        UIHealth uiHealth = FindObjectOfType<UIHealth>();
-        uiHealth.UpdateHealth(health);
+
+        damageEvent.Invoke(health);
     }
 }
--- Assets/Scripts/UIHealth.cs
+++ Assets/Scripts/UIHealth.cs
@@ -5,6 +5,17 @@ public class UIHealth : MonoBehaviour
 {
     public Image healthValue;
 
+    private void OnEnable()
+    {
+        PlayerHealthController.damageEvent = new UnityEngine.Events.UnityEvent<float>();
+        PlayerHealthController.damageEvent.AddListener(UpdateHealth);
+    }
+
+    private void OnDisable()
+    {
+        PlayerHealthController.damageEvent.RemoveListener(UpdateHealth);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
